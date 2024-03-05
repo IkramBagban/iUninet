9535a327f9227e1b67f184ee9658f376de56b70d
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-03-05 20:49:21 +0530

    create sidebar of chat section with some dummy user

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
       </header>
 
       <div className="flex flex-1">
-        <aside className="w-[7rem] bg-white border-r border-gray-200">
+        <aside className=" bg-white border-r border-gray-200">
           <nav className="py-4">
             <NavLink
               to="/chat"
@@ -20,7 +20,7 @@ const Layout = () => {
               }
             >
               <span className="material-symbols-outlined">chat</span>
-              <span>Chat</span>
+              {/* <span>Chat</span> */}
             </NavLink>
             <NavLink
               to="/note"
@@ -31,12 +31,13 @@ const Layout = () => {
               }
             >
               <span className="material-symbols-outlined">note_alt</span>
-              <span>Notes</span>
+              {/* <span>Notes</span> */}
             </NavLink>
           </nav>
         </aside>
 
         <main className="w-5/6  border-l-1  ">
+        {/* <main className="border-l-1  "> */}
           <Outlet />
         </main>
       </div>
